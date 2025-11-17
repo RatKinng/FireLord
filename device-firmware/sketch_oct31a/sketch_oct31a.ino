424d8c907c23ae0d3e4a43cfbac89afc7c1bd039
RatKinng <mjfronek@alaska.edu>
2025-11-17 11:05:19 -0900

    Device PCB

---
+++ device-firmware/sketch_oct31a/sketch_oct31a.ino
@@ -0,0 +1,12 @@
+#include "I2C_COMMS.h"
+#include "UART_COMMS.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
